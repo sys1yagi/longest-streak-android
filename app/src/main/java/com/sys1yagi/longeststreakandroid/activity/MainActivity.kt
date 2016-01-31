f559e65de7c2ec64583cc23c10a93c7f709e1b70
sys1yagi <sylc.yagi@gmail.com>
2016-01-31 14:56:44 +0900

    Fix lint.

--- app/src/main/java/com/sys1yagi/longeststreakandroid/activity/MainActivity.kt
+++ app/src/main/java/com/sys1yagi/longeststreakandroid/activity/MainActivity.kt
@@ -2,6 +2,7 @@ package com.sys1yagi.longeststreakandroid.activity
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.View
 import com.cookpad.android.rxt4a.schedulers.AndroidSchedulers
 import com.sys1yagi.longeststreakandroid.R
@@ -34,11 +35,11 @@ class MainActivity : RxAppCompatActivity() {
                                     System.currentTimeMillis(),
                                     events);
                             if (count > 0) {
-                                binding.contentMain.todayStatus.setTextColor(getColor(R.color.green))
+                                binding.contentMain.todayStatus.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.green))
                                 binding.contentMain.todayStatus.setText(R.string.ok)
                                 binding.contentMain.contributionCount.text = getString(R.string.contribution_count, count)
                             } else {
-                                binding.contentMain.todayStatus.setTextColor(getColor(R.color.red))
+                                binding.contentMain.todayStatus.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.red))
                                 binding.contentMain.todayStatus.setText(R.string.not_yet)
                                 binding.contentMain.contributionCount.visibility = View.GONE
                             }
