627a7bd86a3e003aed4a119e2bcd9b6e3b85ade2
sys1yagi <sylc.yagi@gmail.com>
2016-02-01 12:16:10 +0900

    Show error.

--- app/src/main/java/com/sys1yagi/longeststreakandroid/activity/MainActivity.kt
+++ app/src/main/java/com/sys1yagi/longeststreakandroid/activity/MainActivity.kt
@@ -14,38 +14,70 @@ import com.trello.rxlifecycle.components.support.RxAppCompatActivity
 import rx.schedulers.Schedulers
 
 class MainActivity : RxAppCompatActivity() {
-
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val publicContributionJudgement = PublicContributionJudgement()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setSupportActionBar(binding.toolbar)
+        checkContributionOfTheToday("sys1yagi")
+    }
 
-        binding.contentMain.statusBoard.visibility = View.GONE;
-        GithubService.client.userEvents("sys1yagi")
+    fun showProgress() {
+        binding.contentMain.statusBoard.visibility = View.GONE
+        binding.contentMain.progress.visibility = View.VISIBLE
+        binding.contentMain.error.visibility = View.GONE
+    }
+
+    fun showStatus() {
+        binding.contentMain.statusBoard.visibility = View.VISIBLE
+        binding.contentMain.progress.visibility = View.GONE
+        binding.contentMain.error.visibility = View.GONE
+    }
+
+    fun showError(error: Throwable) {
+        binding.contentMain.statusBoard.visibility = View.GONE
+        binding.contentMain.progress.visibility = View.GONE
+        binding.contentMain.error.visibility = View.VISIBLE
+        binding.contentMain.errorText.text = error.message
+    }
+
+    fun checkContributionOfTheToday(name: String) {
+        showProgress()
+
+        GithubService.client.userEvents(name)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .compose(bindToLifecycle<List<Event>>())
                 .subscribe(
-                        { events ->
-                            binding.contentMain.statusBoard.visibility = View.VISIBLE
-                            binding.contentMain.loading.visibility = View.GONE
-                            val count = publicContributionJudgement.todayContributionCount("sys1yagi",
-                                    System.currentTimeMillis(),
-                                    events);
-                            if (count > 0) {
-                                binding.contentMain.todayStatus.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.green))
-                                binding.contentMain.todayStatus.setText(R.string.ok)
-                                binding.contentMain.contributionCount.text = getString(R.string.contribution_count, count)
-                            } else {
-                                binding.contentMain.todayStatus.setTextColor(ContextCompat.getColor(this@MainActivity, R.color.red))
-                                binding.contentMain.todayStatus.setText(R.string.not_yet)
-                                binding.contentMain.contributionCount.visibility = View.GONE
-                            }
-                        },
-                        { error ->
-                            error.printStackTrace()
-                        })
+                        { events -> setupStatus(name, events) },
+                        { error -> showError(error) }
+                )
+    }
+
+    fun setupStatus(name: String, events: List<Event>) {
+        showStatus()
+
+        val publicContributionJudgement = PublicContributionJudgement()
+        val count = publicContributionJudgement.todayContributionCount(name,
+                System.currentTimeMillis(),
+                events);
+        if (count > 0) {
+            alreadyContributed(count)
+        } else {
+            notYetContributed()
+        }
+    }
+
+    fun alreadyContributed(count: Int) {
+        binding.contentMain.todayStatus.setTextColor(ContextCompat.getColor(this, R.color.green))
+        binding.contentMain.todayStatus.setText(R.string.ok)
+        binding.contentMain.contributionCount.text = getString(R.string.contribution_count, count)
+    }
+
+    fun notYetContributed() {
+        binding.contentMain.todayStatus.setTextColor(ContextCompat.getColor(this, R.color.red))
+        binding.contentMain.todayStatus.setText(R.string.not_yet)
+        binding.contentMain.contributionCount.visibility = View.GONE
     }
 }
