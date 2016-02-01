7d58237e18866d3796c8e8aa38564496b69bb357
sys1yagi <sylc.yagi@gmail.com>
2016-02-01 20:50:29 +0900

    Introduce fragment.

--- app/src/main/java/com/sys1yagi/longeststreakandroid/activity/MainActivity.kt
+++ app/src/main/java/com/sys1yagi/longeststreakandroid/activity/MainActivity.kt
@@ -2,16 +2,11 @@ package com.sys1yagi.longeststreakandroid.activity
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v4.content.ContextCompat
-import android.view.View
-import com.cookpad.android.rxt4a.schedulers.AndroidSchedulers
 import com.sys1yagi.longeststreakandroid.R
-import com.sys1yagi.longeststreakandroid.api.GithubService
 import com.sys1yagi.longeststreakandroid.databinding.ActivityMainBinding
-import com.sys1yagi.longeststreakandroid.model.Event
-import com.sys1yagi.longeststreakandroid.tool.PublicContributionJudgement
+import com.sys1yagi.longeststreakandroid.db.Account
+import com.sys1yagi.longeststreakandroid.fragment.MainFragmentCreator
 import com.trello.rxlifecycle.components.support.RxAppCompatActivity
-import rx.schedulers.Schedulers
 
 class MainActivity : RxAppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -20,64 +15,17 @@ class MainActivity : RxAppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         setSupportActionBar(binding.toolbar)
-        checkContributionOfTheToday("sys1yagi")
-    }
+        if (savedInstanceState == null) {
 
-    fun showProgress() {
-        binding.contentMain.statusBoard.visibility = View.GONE
-        binding.contentMain.progress.visibility = View.VISIBLE
-        binding.contentMain.error.visibility = View.GONE
-    }
+            //for develop
+            val account = Account()
+            account.name = "sys1yagi"
 
-    fun showStatus() {
-        binding.contentMain.statusBoard.visibility = View.VISIBLE
-        binding.contentMain.progress.visibility = View.GONE
-        binding.contentMain.error.visibility = View.GONE
-    }
-
-    fun showError(error: Throwable) {
-        binding.contentMain.statusBoard.visibility = View.GONE
-        binding.contentMain.progress.visibility = View.GONE
-        binding.contentMain.error.visibility = View.VISIBLE
-        binding.contentMain.errorText.text = error.message
-    }
-
-    fun checkContributionOfTheToday(name: String) {
-        showProgress()
-
-        GithubService.client.userEvents(name)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .compose(bindToLifecycle<List<Event>>())
-                .subscribe(
-                        { events -> setupStatus(name, events) },
-                        { error -> showError(error) }
-                )
-    }
-
-    fun setupStatus(name: String, events: List<Event>) {
-        showStatus()
-
-        val publicContributionJudgement = PublicContributionJudgement()
-        val count = publicContributionJudgement.todayContributionCount(name,
-                System.currentTimeMillis(),
-                events);
-        if (count > 0) {
-            alreadyContributed(count)
-        } else {
-            notYetContributed()
+            supportFragmentManager.beginTransaction()
+                    .add(R.id.content_frame, MainFragmentCreator.newBuilder(account).build())
+                    .commit()
         }
     }
 
-    fun alreadyContributed(count: Int) {
-        binding.contentMain.todayStatus.setTextColor(ContextCompat.getColor(this, R.color.green))
-        binding.contentMain.todayStatus.setText(R.string.ok)
-        binding.contentMain.contributionCount.text = getString(R.string.contribution_count, count)
-    }
 
-    fun notYetContributed() {
-        binding.contentMain.todayStatus.setTextColor(ContextCompat.getColor(this, R.color.red))
-        binding.contentMain.todayStatus.setText(R.string.not_yet)
-        binding.contentMain.contributionCount.visibility = View.GONE
-    }
 }
---
+++ app/src/main/java/com/sys1yagi/longeststreakandroid/fragment/MainFragment.kt
@@ -0,0 +1,100 @@
+package com.sys1yagi.longeststreakandroid.fragment
+
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cookpad.android.rxt4a.schedulers.AndroidSchedulers
+import com.sys1yagi.fragmentcreator.annotation.Args
+import com.sys1yagi.fragmentcreator.annotation.FragmentCreator
+import com.sys1yagi.longeststreakandroid.R
+import com.sys1yagi.longeststreakandroid.api.GithubService
+import com.sys1yagi.longeststreakandroid.databinding.FragmentMainBinding
+import com.sys1yagi.longeststreakandroid.db.Account
+import com.sys1yagi.longeststreakandroid.model.Event
+import com.sys1yagi.longeststreakandroid.tool.PublicContributionJudgement
+import com.trello.rxlifecycle.components.support.RxFragment
+import rx.schedulers.Schedulers
+
+@FragmentCreator
+class MainFragment : RxFragment() {
+
+    @Args
+    lateinit var account: Account
+
+    lateinit var binding: FragmentMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        MainFragmentCreator.read(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentMainBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkContributionOfTheToday(account.name)
+    }
+
+    fun showProgress() {
+        binding.statusBoard.visibility = View.GONE
+        binding.progress.visibility = View.VISIBLE
+        binding.error.visibility = View.GONE
+    }
+
+    fun showStatus() {
+        binding.statusBoard.visibility = View.VISIBLE
+        binding.progress.visibility = View.GONE
+        binding.error.visibility = View.GONE
+    }
+
+    fun showError(error: Throwable) {
+        binding.statusBoard.visibility = View.GONE
+        binding.progress.visibility = View.GONE
+        binding.error.visibility = View.VISIBLE
+        binding.errorText.text = error.message
+    }
+
+    fun checkContributionOfTheToday(name: String) {
+        showProgress()
+
+        GithubService.client.userEvents(name)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .compose(bindToLifecycle<List<Event>>())
+                .subscribe(
+                        { events -> setupStatus(name, events) },
+                        { error -> showError(error) }
+                )
+    }
+
+    fun setupStatus(name: String, events: List<Event>) {
+        showStatus()
+
+        val publicContributionJudgement = PublicContributionJudgement()
+        val count = publicContributionJudgement.todayContributionCount(name,
+                System.currentTimeMillis(),
+                events);
+        if (count > 0) {
+            alreadyContributed(count)
+        } else {
+            notYetContributed()
+        }
+    }
+
+    fun alreadyContributed(count: Int) {
+        binding.todayStatus.setTextColor(ContextCompat.getColor(context, R.color.green))
+        binding.todayStatus.setText(R.string.ok)
+        binding.contributionCount.text = getString(R.string.contribution_count, count)
+    }
+
+    fun notYetContributed() {
+        binding.todayStatus.setTextColor(ContextCompat.getColor(context, R.color.red))
+        binding.todayStatus.setText(R.string.not_yet)
+        binding.contributionCount.visibility = View.GONE
+    }
+}
