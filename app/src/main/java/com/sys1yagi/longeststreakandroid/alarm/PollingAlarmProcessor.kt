8d49a13ae7285ed2de224bb8da8cc707e252cc15
sys1yagi <sylc.yagi@gmail.com>
2016-02-02 00:03:17 +0900

    Update alarm-manager-processor.

---
+++ app/src/main/java/com/sys1yagi/longeststreakandroid/alarm/PollingAlarmProcessor.kt
@@ -0,0 +1,13 @@
+package com.sys1yagi.longeststreakandroid.alarm
+
+import android.content.Context
+import android.content.Intent
+import com.sys1yagi.android.alarmmanagersimplify.AlarmProcessor
+import com.sys1yagi.android.alarmmanagersimplify.annotation.Simplify
+
+@Simplify(value = "polling")
+class PollingAlarmProcessor : AlarmProcessor {
+    override fun process(context: Context, intent: Intent) {
+
+    }
+}
