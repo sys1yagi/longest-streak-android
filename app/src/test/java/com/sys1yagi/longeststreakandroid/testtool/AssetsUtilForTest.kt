ae87d3dc933c2c2ebec8ea341a787c3e03de3b01
sys1yagi <sylc.yagi@gmail.com>
2016-02-06 10:36:22 +0900

    Update kotlin version.

--- app/src/test/java/com/sys1yagi/longeststreakandroid/testtool/AssetsUtilForTest.kt
+++ app/src/test/java/com/sys1yagi/longeststreakandroid/testtool/AssetsUtilForTest.kt
@@ -1,12 +1,13 @@
 package com.sys1yagi.longeststreakandroid.testtool
 
 import java.io.File
+import java.nio.charset.Charset
 
 object AssetsUtilForTest {
 
     val PATH = "src/test/assets"
 
     fun readString(fileName: String): String {
-        return File(PATH, fileName).inputStream().bufferedReader("utf-8").readText()
+        return File(PATH, fileName).inputStream().bufferedReader(Charset.forName("utf-8")).readText()
     }
 }
